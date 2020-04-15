3ff8f037dadccbe5749acf0e0afab9a13b7cb003
Абдулла <49492759+SencoPan@users.noreply.github.com>
2020-04-15 14:37:24 +0300

    Helmet setup && Auth validation

--- server/app.js
+++ server/app.js
@@ -4,17 +4,38 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-//const csrf = require("csurf");
+const serveStatic = require("serve-static");
+const session = require("express-session");
+const passport = require("passport");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helmet = require("helmet");
+
+const database = require("./models");
 
 const app = express();
 
-//csrf setup
-//const csrfProtection = csrf({ cookie: true });
+//Auth session protection
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+const isLoggedIn = function(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+};
 
+app.use(
+  session({
+    secret: "keyboard cat",
+    store: new SequelizeStore({
+      db: database.sequelize
+    }),
+    resave: false, // we support the touch method so per the express-session docs this should be set to false
+    proxy: true, // if you do SSL outside of node.
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(helmet());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,13 +43,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "https://postponements.herokuapp.com/",
     credentials: true
   })
 );
-//app.use(csrfProtection);
 
-require("./routes/")(app);
+require("./routes/")(app, passport, isLoggedIn);
+require("./config/passport")(passport, database.admin);
+
+app.use(serveStatic(__dirname + "/dist"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
